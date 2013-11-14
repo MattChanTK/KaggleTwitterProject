5928176fd214f7e0742910ed628333f39ecbd48e
tuzzer <matthew.tk.chan@gmail.com>
2013-11-14 16:00:50 -0500

    Extract tweet and the labels from CSV into lists.

--- KaggleTwitterProject_Sentiment/main.py
+++ KaggleTwitterProject_Sentiment/main.py
@@ -1,6 +1,26 @@
 import csv
+import numpy as np
 
-with open('../Data/train.csv', 'rb') as train_csv:
-    train_data = csv.reader(train_csv, delimiter=',', quotechar='"')
-    for row in train_data:
-        print ', '.join(row)
+
+train_csv = open('../Data/train.csv', 'rb')
+train_data = csv.reader(train_csv, delimiter=',', quotechar='"')
+
+train_sample = list(train_data)
+
+
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+train_s = []
+for row in train_sample:
+    train_s.append(row[4:9])
+train_tweet = column(train_sample, 1)
+train_num_sample = len(train_tweet)
+
+for p in train_s:
+    print p
+
+for e in train_tweet:
+    print e
+
+
