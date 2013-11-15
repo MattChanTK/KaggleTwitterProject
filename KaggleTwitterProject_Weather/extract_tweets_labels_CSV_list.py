75d906b71b54ad13bb121532d34e9c6cfb1876a3
Harsh <harshshah711@gmail.com>
2013-11-14 21:52:58 -0500

    extract tweets and labels from CSV and coverts into list

---
+++ KaggleTwitterProject_Weather/extract_tweets_labels_CSV_list.py
@@ -0,0 +1,27 @@
+__author__ = 'Harsh'
+
+
+import csv
+import numpy as np
+
+
+train_csv = open('../Data/train.csv', 'rb')
+train_data = csv.reader(train_csv, delimiter=',', quotechar='"')
+
+train_sample = list(train_data)
+
+
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+train_s = []
+for row in train_sample:
+    train_s.append(row[4:9])
+train_tweet = column(train_sample, 1)
+train_num_sample = len(train_tweet)
+
+for p in train_s:
+    print p
+
+for e in train_tweet:
+    print e
