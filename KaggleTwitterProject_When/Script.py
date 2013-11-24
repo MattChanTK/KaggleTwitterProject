5ab9fae40e0b666b80d9392f61d7cef2727eefe7
ivars31 <ivars31@gmaill.com>
2013-11-24 01:36:41 -0500

    Best Score so far

--- KaggleTwitterProject_When/Script.py
+++ KaggleTwitterProject_When/Script.py
@@ -3,6 +3,8 @@
 import spell
 import pandas as p
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy import sparse
 import nltk
 
 paths = ['C:/MTE/train.csv', 'C:/MTE/test.csv'] #importing the data
@@ -10,24 +12,36 @@ t = p.read_csv(paths[0])
 t2 = p.read_csv(paths[1])
 #print t # display the data from the
 
+cleanTweetsTrain=t['tweet']
+for Word in cleanTweetsTrain:
+    Word.translate(None,'!`~$%^&*()-_=+[]{}":;?/')#still to clean # and @
+    Word.replace('@mention','')#Still need to clean #...
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-test2 = np.load('test.npy') # Apply the transformation to the test data
-#counts how many of verbs, nouns, etc are there
-X2=np.load('X.npy')
+cleanTweetsTest=t2['tweet']
+for Word in cleanTweetsTrain:
+    Word.translate(None,'!`~$%^&*()-_=+[]{}":;?/')#still to clean # and @
+    Word.replace('@mention', '') #still need to clean #...
 
 tfidf = TfidfVectorizer(max_features=15000, strip_accents='unicode', analyzer='word')#Max features-maximum number of words obtained,
 #Strip_accents=remove accents on all the words, analyzer-generate features based on words.
-tfidf.fit(t['tweet']) #Learn conversion law. Apply Tf-Idf on the tweet column
-X = tfidf.transform(t['tweet'])# Transform raw text documents to tfidf vectors. Learn a conversion law from documents to array data
-test = tfidf.transform(t2['tweet']) # Apply the transformation to the test data
+tfidf.fit(cleanTweetsTrain) #Learn conversion law. Apply Tf-Idf on the tweet column
+X = tfidf.transform(cleanTweetsTrain)# Transform raw text documents to tfidf vectors. Learn a conversion law from documents to array data
+test = tfidf.transform(cleanTweetsTest) # Apply the transformation to the test data
 y = np.array(t.ix[:, 4:])# ix divides the data in columns
 
+##new data
+#test2= sparse.csr_matrix(np.load('test.npy')) # Apply the transformation to the test data
+#test=sparse.hstack([test_initial, test2])
+#X2 = sparse.csr_matrix(np.load('X.npy'))
+#X = sparse.hstack([Y, X2])
 #algorithm used to solve
 from sklearn import linear_model
-clf =linear_model.Ridge(alpha=.5)
-clf.fit(X2+X,y)
-test_prediction = clf.predict(test2+test)
+clf =linear_model.Ridge(alpha=.2)
+clf.fit(X,y)
+test_prediction = clf.predict(test)
+
+#cleaning negative values
+test_prediction = test_prediction.clip(0.0,1.0)
 
 #print error
 print 'Train error: {0}'.format(np.sqrt(np.sum(np.array(np.array(clf.predict(X))-y)**2)/(X.shape[0]*24.0)))#
@@ -37,7 +51,7 @@ prediction = np.array(np.hstack([np.matrix(t2['id']).T, test_prediction]))#array
 
 #matrix: creates a matrix (increase from 1 to more dimensions. T:transpose
 col = '%i,' + '%f,'*23+ '%f'
-np.savetxt('C:/MTE/prediction3.csv', prediction, col, delimiter=',')
+np.savetxt('C:/MTE/prediction6.csv', prediction, col, delimiter=',')
 
 #import re, collections
 #
